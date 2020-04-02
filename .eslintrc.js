1a8aaf9f387307c47cd06edb4551ac56feec56c2
devboell <devboell@gmail.com>
2020-04-02 10:38:47 +0200

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: ["airbnb", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module",
+    jsx: true
+  },
+  rules: {
+    semi: ["error", "never"]
+    //  "global-require": ["off"],
+    //  "no-plusplus": ["off"],
+    //  "arrow-parens": ["error", "as-needed"],
+    //  "no-restricted-syntax": ["off"],
+    //  "no-console": ["warn", { allow: ["error"] }],
+    //  "no-continue": ["off"],
+    //  "import/no-unresolved": [2, {}],
+    //  "import/prefer-default-export": ["off"],
+    //  "import/no-named-as-default": ["off"],
+    //  "import/no-named-as-default-member": ["off"],
+    //  "class-methods-use-this": ["off"],
+    //  "eol-last": ["error", "always"],
+    //  "react/forbid-prop-types": ["off"],
+    //  "react/jsx-one-expression-per-line": ["warning", { allow: "single-child" }],
+    //  "no-underscore-dangle": ["off"],
+    //  "react/require-default-props": ["off"],
+    //  "react/no-array-index-key": ["off"],
+    //  "react/no-did-update-set-state": ["off"],
+    //  "jsx-a11y/label-has-associated-control": ["off"],
+    //  "jsx-a11y/label-has-for": ["off"],
+    //  "max-len": ["off"]
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  settings: {
+    "import/resolver": {
+      webpack: {
+        config: "webpack.common.js"
+      }
+    }
+  }
+};
