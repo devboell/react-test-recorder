56f7bfd65a6b564b822b39f5b5241c6d945eb9bc
devboell <devboell@gmail.com>
2020-04-05 11:20:37 +0200

    configure headers and middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const { writeTest } = require('./write-test')
 
 const app = express()
 
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
+
 app.post('/recording', (req, res) => {
-  console.log(req.body)
+  // console.log('req.body', JSON.stringify(req.body, null, 2))
+  writeTest(req.body)
   res.sendStatus(200)
 })
 
