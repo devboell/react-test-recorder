2e2ef45611846207ef81c38ad44282059efa5f1a
devboell <devboell@gmail.com>
2020-06-24 07:22:20 +0200

    remove old tests

--- src/server/tests/write-test.test.js
+++
@@ -1,7 +0,0 @@
-const { fileContents } = require('../write-test')
-const mockRecording = require('./mocks/signIn.json')
-
-it('should ', () => {
-  const received = fileContents(mockRecording)
-  expect(received).toMatchSnapshot()
-})
