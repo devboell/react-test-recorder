cdb75552e16a297970d9f29dd0c7f1b64bc707bc
devboell <devboell@gmail.com>
2020-06-24 07:22:35 +0200

    changed comment

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 // TODO: check this again!
 // it is meant to allow babel to compile files that
-// are imported by the examples sub-packages from upper
-// scope. In this case the Recorder component.
+// are imported by the examples sub-packages from their
+// upper scope. In this case the Recorder component.
 
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
